4db071a4f42b5ed69c909dea0d2a0318858729c6
Freilin Manzano <freilin.manzano@externos.bci.cl>
2022-03-09 16:24:09 -0300

    add questions for node.

---
+++ lib/data/node_questions.dart
@@ -0,0 +1,92 @@
+const nodeQuestions = [
+  {
+    'question':
+        "Which of the following statements is valid to import a module in file?",
+    'options': [
+      {'option': 'var fs = require("fs");', 'correct': true},
+      {'option': 'var fs = import("fs");', 'correct': false},
+      {'option': 'package fs;', 'correct': false},
+      {'option': 'import fs;', 'correct': false}
+    ]
+  },
+  {
+    'question': "What is the fullform of NPM?",
+    'options': [
+      {'option': 'Node Package Manager', 'correct': true},
+      {'option': 'New Package Manager', 'correct': false},
+      {'option': 'Node Project Manager', 'correct': false},
+      {'option': 'None', 'correct': false}
+    ]
+  },
+  {
+    'question': "Is Node multithreaded?",
+    'options': [
+      {'option': 'False', 'correct': true},
+      {'option': 'True', 'correct': false}
+    ]
+  },
+  {
+    'question': "How to make node modules available externally?",
+    'options': [
+      {'option': 'module.export', 'correct': true},
+      {'option': 'module.expose', 'correct': false},
+      {'option': 'module.spread', 'correct': false},
+      {'option': 'none', 'correct': false}
+    ]
+  },
+  {
+    'question': "Which of the following are Node.js stream types?",
+    'options': [
+      {'option': 'All', 'correct': true},
+      {'option': 'Duplex', 'correct': false},
+      {'option': 'Readable', 'correct': false},
+      {'option': 'Writable', 'correct': false},
+      {'option': 'None', 'correct': false}
+    ]
+  },
+  {
+    'question': "What is the default scope of Node.js application?",
+    'options': [
+      {'option': 'Local', 'correct': true},
+      {'option': 'Global', 'correct': false},
+      {'option': 'Public', 'correct': false},
+      {'option': 'Private', 'correct': false}
+    ]
+  },
+  {
+    'question': "Which module is used to serve static files in Node.js?",
+    'options': [
+      {'option': 'node-static', 'correct': true},
+      {'option': 'static', 'correct': false},
+      {'option': 'http', 'correct': false},
+      {'option': 'node-http', 'correct': false}
+    ]
+  },
+  {
+    'question': "What is the use of underscore variable in REPL session?",
+    'options': [
+      {'option': 'To get the last result', 'correct': true},
+      {'option': 'To get the last used command', 'correct': false},
+      {'option': 'To store the result', 'correct': false},
+      {'option': 'None', 'correct': false}
+    ]
+  },
+  {
+    'question': "How many Node object methods are available?",
+    'options': [
+      {'option': '18', 'correct': true},
+      {'option': '20', 'correct': false},
+      {'option': '22', 'correct': false},
+      {'option': '24', 'correct': false}
+    ]
+  },
+  {
+    'question': "How to check equality of two nodes?",
+    'options': [
+      {'option': 'isEqualNode()', 'correct': true},
+      {'option': '==', 'correct': false},
+      {'option': '===', 'correct': false},
+      {'option': 'isEqual()', 'correct': false}
+    ]
+  }
+];
--- lib/data/questions_list.dart
+++ lib/data/questions_list.dart
@@ -1,20 +1,20 @@
+import 'package:fquiz/data/aws_questions.dart';
+import 'package:fquiz/data/html_questions.dart';
 import 'package:fquiz/data/java_questions.dart';
+import 'package:fquiz/data/js_questions.dart';
 import 'package:fquiz/data/mongo_questions.dart';
+import 'package:fquiz/data/node_questions.dart';
 import 'package:fquiz/data/react_questions.dart';
 import 'package:fquiz/data/spring_questions.dart';
 import 'package:fquiz/data/sql_questions.dart';
 
-import 'aws_questions.dart';
-import 'html_questions.dart';
-import 'js_questions.dart';
-
 var questionsList = {
-  'javascript': [...jsQuestions],
+  'javascript': [...jsQuestions, ...nodeQuestions],
   'angular': [],
   'database': [...sqlQuestions, ...mongoQuestions],
   'react': [...reactQuestions],
   'html5': [...htmlQuestions],
   'aws': [...awsQuestions],
   'java': [...javaQuestions],
-  'spring': [...springQuestions],
+  'spring': [...springQuestions]
 };
