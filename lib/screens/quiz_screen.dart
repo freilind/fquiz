808570f61d038fd1de06c2cd06e5a92f4a8c0998
Freilin Manzano <freilin.manzano@externos.bci.cl>
2022-03-09 21:05:07 -0300

    add icon category in bar.

--- lib/screens/quiz_screen.dart
+++ lib/screens/quiz_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:fquiz/models/category.dart';
 import 'package:fquiz/models/question.dart';
+import 'package:fquiz/providers/category_provider.dart';
 import 'package:fquiz/providers/questions_provider.dart';
 import 'package:fquiz/widgets/pageview_content_widget.dart';
 import 'package:provider/provider.dart';
@@ -8,6 +10,7 @@ class QuizScreen extends StatelessWidget {
   QuizScreen({Key? key}) : super(key: key);
   static const routeName = '/quiz';
   late List<Question> _questions = [];
+  late Category _category;
   final PageController _controller = PageController(viewportFraction: 0.9);
 
   @override
@@ -41,18 +44,35 @@ class QuizScreen extends StatelessWidget {
                   )),
                 )),
             appBar: AppBar(
-              title: Text('FQu!z',
-                  style: Theme.of(context)
-                      .textTheme
-                      .headline4), //color: Colors.white,
+              title:
+                  Text('FQu!z', style: Theme.of(context).textTheme.headline4),
+              actions: [
+                Container(
+                  margin: const EdgeInsets.only(right: 20.0),
+                  padding: const EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    //borderRadius: BorderRadius.circular(20),
+                    color: Color(_category.color),
+                  ),
+                  child: Icon(IconData(_category.icon,
+                      fontFamily: _category.fontFamily,
+                      fontPackage: 'font_awesome_flutter')),
+                ),
+              ], //color: Colors.white,
             ),
           );
         });
   }
 
   Future<void> _getQuestions(BuildContext context) async {
-    var provider = Provider.of<QuestionsProvider>(context, listen: false);
-    _questions = provider.questions;
-    provider.controller = _controller;
+    var providerQuestion =
+        Provider.of<QuestionsProvider>(context, listen: false);
+    _questions = providerQuestion.questions;
+    providerQuestion.controller = _controller;
+
+    var providerCategory =
+        Provider.of<CategoryProvider>(context, listen: false);
+    _category = providerCategory.category;
   }
 }
